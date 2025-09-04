e63f9b94ffa7097f3a85d24483dfc9a76786ee54
QuillLovesIT <shaquillerules@gmail.com>
2025-09-04 18:31:54 +0700

    1.1 COMPAT UPDATE!!! (new prototype armodrillo texture + compat with the color slider.. hopefully)

--- addon/andromedan_wonders/kubejs_scripts/alien_codex.js
+++ addon/andromedan_wonders/kubejs_scripts/alien_codex.js
@@ -8,21 +8,21 @@ global.alienevo_background_190 = ['minecraft:textures/block/stone.png']; // Tile
 
 global.alienevo_190_10k_uniforms = [true, false, false]; // Glow (enabled), Primary (enabled), Secondary (enabled)
 global.alienevo_190_default_uniforms = [true, false, false]; // Glow (enabled), Primary (disabled), Secondary (disabled)
-global.alienevo_190_prototype_uniforms = [true, false, false];
+global.alienevo_190_prototype_uniforms = [true, true, true];
 
 // Make sure your colors are listed from BRIGHTEST TO DARKEST. You can use as many or as few colors in a palette as you'd like.
-global.alienevo_190_10k_skincolor_palette_1 = ["doc522", "c7bd25", "c3b921", "a69422", "a49420", "948021", "8d7a20"];
-global.alienevo_190_10k_skincolor_palette_2 = ["6e788c", "6a737e", "5e656f", "555b63", "474e5a", "454b53", "3a4049"];
+global.alienevo_190_10k_skincolor_palette_1 = ["f4f753", "f0e816", "ebd50f", "d9b50e", "c7980d", "b57d0c"];
+global.alienevo_190_10k_skincolor_palette_2 = ["6e788c", "6a737e", "5e656f", "536174", "555b63", "3a4049"];
 
 
 
-global.alienevo_190_default_skincolor_palette_1 = ["doc522", "c7bd25", "c3b921", "a69422", "a49420", "948021", "8d7a20"];
-global.alienevo_190_default_skincolor_palette_2 = ["6e788c", "6a737e", "5e656f", "555b63", "474e5a", "454b53", "3a4049"];
+global.alienevo_190_default_skincolor_palette_1 = ["f4f753", "f0e816", "ebd50f", "d9b50e", "c7980d", "b57d0c"];
+global.alienevo_190_default_skincolor_palette_2 = ["6e788c", "6a737e", "5e656f", "536174", "555b63", "3a4049"];
 
-global.alienevo_190_prototype_skincolor_palette_1 = ["cdc322", "b5a821", "b5a01e", "b0a520", "aca230", "a59d1b", "a49420", "9b8a21", "948021", "9d811a", "90861a", "8d7a20", "88771d"];
-global.alienevo_190_prototype_skincolor_palette_2 = ["6e788c", "6a737e", "5e656f", "555b63", "474e5a", "454b53", "3a4049"];
+global.alienevo_190_prototype_skincolor_palette_1 = ["f4f753", "f0e816", "ebd50f", "d9b50e", "c7980d", "b57d0c"];
+global.alienevo_190_prototype_skincolor_palette_2 = ["7e8691", "747b88", "6e788c", "6c707e", "636575", "536174", "5b5b6b", "545361", "4c515a", "474e5a", "454b53", "434953", "424853", "3a4049"];
 
 
-global.alienevo_300_10k_glowcolor_1 = ["41cb27"];
-global.alienevo_300_default_glowcolor_1 = ["41cb27"];
-global.alienevo_300_prototype_glowcolor_1 = ["41cb27"];
+global.alienevo_190_10k_glowcolor_1 = ["ffffff"];
+global.alienevo_190_default_glowcolor_1 = ["ffffff"];
+global.alienevo_190_prototype_glowcolor_1 = ["ffffff"];
